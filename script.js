33729d5e91ec11b86fa362494c85620c9c645eb8
Nithin525 <141931427+Nithin525@users.noreply.github.com>
2024-11-30 11:06:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ function shortcut(s1, s2) {
     if (!s1 || !s2) {
         return '';
     }
-    return "'" + s1[0] + s2[0] + "'";
+    return s1[0] + s2[0];
 }
 
-// Do not change the code below.
+// Example usage
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(shortcut(s1, s2));
